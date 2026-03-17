50f73482081fcdec93ff74fc50749c3f530d1b88
Lucas Vieira <l.vieirasdev@gmail.com>
2026-03-17 01:20:25 -0300

    Ex Curso em video

---
+++ Ex_015.py
@@ -0,0 +1,3 @@
+c = float(input("Digite o valor da temperatura em Celsius: "))
+f = (c * 9/5) + 32
+print(f"Atemperatura digitada foi : {c}, e convertida fica :{f}")
---
+++ Ex_16.py
@@ -0,0 +1,6 @@
+q_dias = int(input("Digite quantos dias você dicou com o carro : "))
+q_km = float(input("Digite quantos Km Você andou : "))
+v_finaldias = q_dias * 60
+v_kms = q_km * 0.15
+print(f"O total de Kms andados foi de: {q_km},e o total de dias foi : {q_dias}")
+print(f"Você gastou de diaria: {v_finaldias}, e de quilometragem : {v_kms}")
